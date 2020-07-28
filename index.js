36507e67e011670c882ea4050fe6317ebd5828f7
ricardocvel <ricardocampos_pr@live.com>
2020-07-28 14:28:19 -0300

    idex.js - teste

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 
 
+// get, post, put, delete
+
+
 app.get('/', (request, response) => {
-    return response.json({message: 'Hello Word and São José'});
+    return response.json({message: 'teste'});
 });
 
 app.listen(3333);
