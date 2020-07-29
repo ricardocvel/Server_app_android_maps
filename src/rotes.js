9f905b24c4d1be197d325d8c7373d9a19a3dbe85
ricardocvel <ricardocampos_pr@live.com>
2020-07-29 13:33:23 -0300

    envio de dados no banco

--- src/rotes.js
+++ src/rotes.js
@@ -1,16 +1,35 @@
 const { Router } = require("express");
 const axios = require('axios'); 
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
-    const { github_username } = request.body;
+    //obtem do dado
+    const { github_username, techs } = request.body;
 
+    //await identifica a função assincrona + async
     const apiresponse = await axios.get(`https://api.github.com/users/${github_username}`);
     
-    console.log(apiresponse.data);
+    const naobio = 'Nao possui bio';
+    let {name = login, avatar_url, bio = naobio} = apiresponse.data;
+    if (!bio){
+        bio = naobio;
+    }
 
-    return response.json({message: 'teste'});
+    const techsArray = techs.split(',').map(tech => techs.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        tech: techsArray,
+    })
+
+    console.log(name, avatar_url, bio, github_username);
+
+    return response.json(dev);
 });
 
 module.exports = routes;
